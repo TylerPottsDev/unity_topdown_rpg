784900e69129642d186385680e5cd30fbc363257
Tyler Potts <tylerpottsdev@gmail.com>
2021-02-25 14:59:19 +0000

    Bow n Arrowsgit add .

---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAttack : MonoBehaviour {
+    [SerializeField] GameObject ArrowPrefab;
+	[SerializeField] SpriteRenderer ArrowGFX;
+	[SerializeField] Slider BowPowerSlider;
+	[SerializeField] Transform Bow;
+
+	[Range(0, 10)]
+	[SerializeField] float BowPower;
+
+	[Range(0, 3)]
+	[SerializeField] float MaxBowCharge;
+	
+	float BowCharge;
+	bool CanFire = true;
+
+	private void Start() {
+		BowPowerSlider.value = 0f;
+		BowPowerSlider.maxValue = MaxBowCharge;
+	}
+
+	private void Update() {
+		if (Input.GetMouseButton(0) && CanFire) {
+			ChargeBow();
+		} else if (Input.GetMouseButtonUp(0) && CanFire) {
+			FireBow();
+		} else {
+			if (BowCharge > 0f) {
+				BowCharge -= 1f * Time.deltaTime;
+			} else {
+				BowCharge = 0f;
+				CanFire = true;
+			}
+
+			BowPowerSlider.value = BowCharge;
+		}
+	}
+
+	void ChargeBow() {
+		ArrowGFX.enabled = true;
+		
+		BowCharge += Time.deltaTime;
+
+		BowPowerSlider.value = BowCharge;
+
+		if (BowCharge > MaxBowCharge) {
+			BowPowerSlider.value = MaxBowCharge;
+		}
+	}
+
+	void FireBow() {
+		if (BowCharge > MaxBowCharge) BowCharge = MaxBowCharge;
+
+		float ArrowSpeed = BowCharge + BowPower;
+
+		float angle = Utility.AngleTowardsMouse(Bow.position);
+		Quaternion rot = Quaternion.Euler(new Vector3(0f, 0f, angle - 90f));
+
+		Arrow Arrow = Instantiate(ArrowPrefab, Bow.position, rot).GetComponent<Arrow>();
+		Arrow.ArrowVelocity = ArrowSpeed;
+
+		CanFire = false;
+		ArrowGFX.enabled = false;
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,16 +6,23 @@ public class PlayerMovement : MonoBehaviour {
     public float moveSpeed;
 
     public Rigidbody2D rb;
+	[SerializeField] Transform hand;
 
     Vector2 movement;
 
     void Update() {
         MovementInput();
+		RotateHand();
     }
 
     private void FixedUpdate() {
         rb.velocity = movement * moveSpeed;
     }
+
+	void RotateHand() {
+		float angle = Utility.AngleTowardsMouse(hand.position);
+		hand.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+	}
 
     void MovementInput () {
         float mx = Input.GetAxisRaw("Horizontal");
